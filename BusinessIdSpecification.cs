26cd9c076b8a880b0d1e64d947be75fd54f06191
Jimmy Fagerholm <fagerholm.jimmy@gmail.com>
2019-02-05 12:21:44 +0200

    Updated Specifications and made more modulare. Added comments to methods.

--- BusinessIdSpecification.cs
+++ BusinessIdSpecification.cs
@@ -5,21 +5,28 @@ using System.Text.RegularExpressions;
 namespace Specifications
 {
 
+    //  <summary>
+    //  Struct to be associated with y-tunnus.
+    //  As of 2019 Y-tunnus should be of format ddddddd-d
+    //  </summary>
+    //  <param name="y_tunnus">
     public struct BusinessID
     {
         public string y_tunnus;
     }
 
     public class BusinessIdSpecification : ISpecification<BusinessID> {
+        // Specifications for Y-tunnus
         private char Y_TUNNUS_SPLIT_CHAR = '-';
         private readonly int ID_LENGTH = 9;
-        private string pattern = @"^\d{7}-\d{1}$";
+        private readonly int SPLIT_CHAR_INDEX = ID_LENGTH - 2;
+        private string pattern = @"^\d{" + ID_LENGTH-SPLIT_CHAR_INDEX + @"}"+ Y_TUNNUS_SPLIT_CHAR + @"\d{" + ((ID_LENGTH-SPLIT_CHAR_INDEX)+1) + @"}$";
 
-        List<string> dissatisfactionList = new List<string>();
+        List<string> dissatisfactionList;
 
         // Constructor
-        public BusinessIdSpecification()
-        {
+        public BusinessIdSpecification(){
+            dissatisfactionList = new List<string>();
         }
 
         public IEnumerable<string> ReasonsForDissatisfaction
@@ -28,7 +35,7 @@ namespace Specifications
             {
                 if (dissatisfactionList != null)
                 {
-                    foreach (String reason in dissatisfactionList)
+                    foreach (string reason in dissatisfactionList)
                     {
                         yield return reason;
                     }
@@ -36,17 +43,25 @@ namespace Specifications
             }
         }
         
+        //  <summary>
+        //  Check if entity accepted by BusinessIDSpecification
+        //
+        //  </summary>
         public bool IsSatisfiedBy(BusinessID entity)
         {
+            // Check if entity not defined.
+            if (entity == null) return false;
+            // If Y-tunnus is OK, continue.
             if (Regex.Match(entity.y_tunnus, pattern).Success) return true;
             dissatisfactionList = new List<string>();
             // Check length of Y-tunnus
-            if (entity.y_tunnus.Length < ID_LENGTH) dissatisfactionList.Add(entity.y_tunnus + " Y-tunnus is too short.");
-            if (entity.y_tunnus.Length > ID_LENGTH) dissatisfactionList.Add(entity.y_tunnus + " Y-tunnus is too long.");
-            // Check if Y-tunnus is of right format. As of 2019, the format is 1234567-8.
-            if (entity.y_tunnus.Split(Y_TUNNUS_SPLIT_CHAR).Length != 2) dissatisfactionList.Add(entity.y_tunnus + " Y-tunnus format is wrong.");
-            if (!Regex.Match(entity.y_tunnus, pattern).Success) dissatisfactionList.Add(entity.y_tunnus + " Y-tunnus contains illegal characters.");
-
+            if(entity.y_tunnus.Length != ID_LENGTH) if(entity.y_tunnus.Length < ID_LENGTH? dissatisfactionList.Add("Y-tunnus is too short."): dissatisfactionList.Add("Y-tunnus is too long."));
+            else{
+                // Check if format is correct.
+                if(!entity.y_tunnus[Y_TUNNUS_SPLIT_CHAR_INDEX] != Y_TUNNUS_SPLIT_CHAR) dissatisfactionList.Add("Y-tunnus format is wrong.");
+                // if format is correct check for illegal characters, only accept numbers and designated split char. 
+                else if(!Regex.Match(entity.y_tunnus, pattern).Success) dissatisfactionList.Add"Y-tunnus contains illegal characters.");
+            }
             return false;
         }
     }
